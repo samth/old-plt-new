racket/racket

commit 05b88930c0a19b8e8d56433ca8273f635c50793b

improve how an after-insert callback behaves (specifically, avoid redundant work when there are multiple inserts during an edit sequence)
closes PR 12897

collects/drracket/private/rep.rkt
@@ -758,8 +758,8 @@ TODO
         (inner (void) after-insert start len)
         (cond
           [(in-edit-sequence?) 
-           (set! had-an-insert (cons (cons start len) had-an-insert))]
-          [else (update-after-insert start len)]))
+           (set! had-an-insert (cons start had-an-insert))]
+          [else (update-after-inserts (list start))]))
       
       ;; private field
       (define had-an-insert '())
@@ -768,28 +768,14 @@ TODO
         (inner (void) after-edit-sequence)
         (let ([to-clean had-an-insert])
           (set! had-an-insert '())
-          (for-each
-           (lambda (pr)
-             (update-after-insert (car pr) (cdr pr)))
-           to-clean)))
+	  (update-after-inserts to-clean)))
       
-      (define/private (update-after-insert start len)
+      (define/private (update-after-inserts starts)
         (unless inserting-prompt?
           (reset-highlighting))
-        (when (and prompt-position (< start prompt-position))
-          
-          ;; trim extra space, according to preferences
-          #;
-          (let* ([start (get-repl-header-end)]
-                 [end (get-insertion-point)]
-                 [space (- end start)]
-                 [pref (preferences:get 'drracket:repl-buffer-size)])
-            (when (car pref)
-              (let ([max-space (* 1000 (cdr pref))])
-                (when (space . > . max-space)
-                  (let ([to-delete-end (+ start (- space max-space))])
-                    (delete/io start to-delete-end))))))
-          
+        (when (and prompt-position 
+		   (ormap (λ (start) (< start prompt-position))
+			  starts))
           (set! prompt-position (get-unread-start-point))
           (reset-regions (append (all-but-last (get-regions))
                                  (list (list prompt-position 'end))))))

samth/old-plt-new

commit 02a9d15eeb36538fc740bad97367606bf1dd3573

fix a bug introduced by commit 05b88930c0a19b8e8d56433ca8273f635c50793b the symptom of the bug is that the red error highlighting never worked
Please merge to the release branch

collects/drracket/private/rep.rkt
@@ -766,9 +766,10 @@ TODO
       
       (define/augment (after-edit-sequence)
         (inner (void) after-edit-sequence)
-        (let ([to-clean had-an-insert])
-          (set! had-an-insert '())
-	  (update-after-inserts to-clean)))
+        (unless (null? had-an-insert)
+          (let ([to-clean had-an-insert])
+            (set! had-an-insert '())
+            (update-after-inserts to-clean))))
       
       (define/private (update-after-inserts starts)
         (unless inserting-prompt?