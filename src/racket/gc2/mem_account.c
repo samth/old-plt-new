racket/racket

commit aa08a684248d3b49569de434e62f54babe88961c

add phantom byte strings
A phantom byte string is a small object that the memory
manager treats as an arbitrary-sized object, where the
size is specified when the phantom byte string is created
or or when size is changed via `set-phantom-bytes!'.

src/racket/gc2/mem_account.c
@@ -447,6 +447,9 @@ static void BTC_do_accounting(NewGC *gc)
     last = cur;
     while(cur) {
       int owner = custodian_to_owner_set(gc, cur);
+      uintptr_t save_count = gc->phantom_count;
+
+      gc->phantom_count = 0;
 
       gc->current_mark_owner = owner;
       GCDEBUG((DEBUGOUTF,"MARKING THREADS OF OWNER %i (CUST %p)\n", owner, cur));
@@ -458,6 +461,10 @@ static void BTC_do_accounting(NewGC *gc)
 
       last = cur;
       box = cur->global_next; cur = box ? SCHEME_PTR1_VAL(box) : NULL;
+
+      owner_table[owner]->memory_use = add_no_overflow(owner_table[owner]->memory_use, 
+                                                       gcBYTES_TO_WORDS(gc->phantom_count));
+      gc->phantom_count = save_count;
     }
 
     /* walk backward folding totals int parent */
@@ -470,7 +477,8 @@ static void BTC_do_accounting(NewGC *gc)
         int powner = custodian_to_owner_set(gc, parent);
 
         owner_table = gc->owner_table;
-        owner_table[powner]->memory_use += owner_table[owner]->memory_use;
+        owner_table[powner]->memory_use = add_no_overflow(owner_table[powner]->memory_use,
+                                                          owner_table[owner]->memory_use);
         owner_table[powner]->master_memory_use += owner_table[owner]->master_memory_use;
       }
 

collects/scribblings/reference/memory.scrbl
@@ -252,3 +252,50 @@ information in a dump. The information that is available depends on
 your Racket build; check the end of a dump from a particular build to
 see if it offers additional information; otherwise, all @racket[v]s are
 ignored.}
+
+@;------------------------------------------------------------------------
+@section[#:tag "phantom-bytes"]{Phantom Byte Strings}
+
+A @deftech{phantom byte string} is a small Racket value that is
+treated by the Racket memory manager as having an arbitrary size,
+which is specified when the @tech{phantom byte string} is created or
+when it is changed via @racket[set-phantom-bytes!].
+
+A @tech{phantom byte string} acts as a hint to Racket's memory
+manager that memory is allocated within the process but through a
+separate allocator, such as through a foreign library that is accessed
+via @racketmodname[ffi/unsafe]. This hint is used to trigger
+@tech{garbage collections} or to compute the result of
+@racket[current-memory-use]. Currently, the hint is used only in
+Racket 3m (the main variant of Racket).
+
+@defproc[(phantom-bytes? [v any/c]) boolean?]{
+
+Returns @racket[#t] if @racket[v] is a @tech{phantom byte string},
+@racket[#f] otherwise.}
+
+
+@defproc[(make-phantom-bytes [k exact-nonnegative-integer?])
+         phantom-bytes?]{
+
+Creates a @tech{phantom byte string} that is treated by the Racket
+memory manager as being @racket[k] bytes in size. For a large enough
+@racket[k], the @exnraise[exn:fail:out-of-memory]---either because the
+size is implausibly large, or because a memory limit has been
+installed with @racket[custodian-limit-memory].}
+
+
+@defproc[(set-phantom-bytes! [phantom-bstr phantom-bytes?]
+                             [k exact-nonnegative-integer?])
+         phantom-bytes?]{
+
+Adjusts the size of a @tech{phantom byte string} as it is treated by
+the Racket memory manager.
+
+For example, if the memory that @racket[phantom-bstr] represents is
+released through a foreign library, then @racket[(set-phantom-bytes!
+phantom-bstr 0)] can reflect the change in memory use.
+
+When @racket[k] is larger than the current size of
+@racket[phantom-bstr], then this function can raise
+@racket[exn:fail:out-of-memory], like @racket[make-phantom-bytes].}

collects/tests/racket/will.rktl
@@ -186,5 +186,38 @@
   (test #t < n 50))
      
 ;; ----------------------------------------
+;; Phantom bytes:
+
+(when (eq? '3m (system-type 'gc))
+  (define s (make-semaphore))
+  (define c (make-custodian))
+  (define t (parameterize ([current-custodian c])
+              (thread (lambda ()
+                        (semaphore-wait s)
+                        (define b (make-phantom-bytes (expt 2 29)))
+                        (test #t phantom-bytes? b)
+                        (test #f phantom-bytes? 0)
+                        (semaphore-wait s)
+                        (set-phantom-bytes! b 0)
+                        (semaphore-wait s)))))
+  (sync (system-idle-evt))
+  (collect-garbage)
+  (define m (current-memory-use))
+  (define mc (current-memory-use c))
+  (semaphore-post s)
+  (sync (system-idle-evt))
+  (test #t > (current-memory-use) (+ m (expt 2 28)))
+  (collect-garbage)
+  (test #t > (current-memory-use) (+ m (expt 2 28)))
+  (test #t > (current-memory-use c) (+ mc (expt 2 28)))
+  (semaphore-post s)
+  (sync (system-idle-evt))
+  (test #t < (current-memory-use) (+ m (expt 2 28)))
+  (collect-garbage)
+  (test #t < (current-memory-use) (+ m (expt 2 28)))
+  (test #t < (current-memory-use c) (+ mc (expt 2 28)))
+  (semaphore-post s))
+
+;; ----------------------------------------
 
 (report-errs)

src/racket/gc2/gc2.h
@@ -91,7 +91,8 @@ GC2_EXTERN void GC_add_roots(void *start, void *end);
    `start' (inclusive) and `end' (exclusive) contains pointers. */
 
 GC2_EXTERN void GC_init_type_tags(int count, int pair, int mutable_pair, int weakbox, 
-                                  int ephemeron, int weakarray, int custbox);
+                                  int ephemeron, int weakarray, int custbox,
+                                  int phantom);
 /*
    Called by Racket to indicate the number of different type tags it
    uses, starting from 0. `count' is always less than 256. The weakbox
@@ -275,6 +276,11 @@ GC2_EXTERN int GC_is_on_allocated_page(void *p);
    Returns 1 if p refers to a page of memory on which
    the GC allocates objects (although p may or may not
    be a valid pointer to the start of an alloctaed object). */
+
+GC2_EXTERN int GC_allocate_phantom_bytes(intptr_t);
+/* 
+   Returns 0 if allocation should fail due to a memory limit, 
+   1 otherwise. */
 
 /***************************************************************************/
 /* Memory tracing                                                          */

src/racket/gc2/newgc.c
@@ -1492,6 +1492,44 @@ intptr_t GC_alloc_alignment()
 
 intptr_t GC_malloc_stays_put_threshold() { return MAX_OBJECT_SIZE; }
 
+uintptr_t add_no_overflow(uintptr_t a, uintptr_t b)
+{
+  uintptr_t c = a + b;
+
+  if (c < a)
+    c = (uintptr_t)-1;
+
+  return c;
+}
+
+int GC_allocate_phantom_bytes(intptr_t request_size_bytes)
+{
+#ifdef NEWGC_BTC_ACCOUNT
+  NewGC *gc = GC_get_GC();
+
+  if (premaster_or_place_gc(gc)) {
+    if (BTC_single_allocation_limit(gc, request_size_bytes))
+      return 0;
+  }
+#endif
+
+  if ((request_size_bytes > 0)
+      && ((gc->phantom_count + request_size_bytes) < gc->phantom_count))
+    /* overflow */
+    return 1;
+
+  gc->phantom_count += request_size_bytes;
+  /* adjust `gc->memory_in_use', but protect against {over,under}flow: */
+  if (request_size_bytes < 0) {
+    request_size_bytes = -request_size_bytes;
+    if (gc->memory_in_use > request_size_bytes)
+      gc->memory_in_use -= request_size_bytes;
+  } else
+    gc->memory_in_use = add_no_overflow(gc->memory_in_use, request_size_bytes);
+
+  return 1;
+}
+
 void GC_create_message_allocator() {
   NewGC *gc = GC_get_GC();
   Allocator *a;
@@ -1769,8 +1807,10 @@ inline static void master_set_max_size(NewGC *gc)
 inline static void reset_nursery(NewGC *gc)
 {
   uintptr_t new_gen0_size;
+  
   new_gen0_size = NUM((GEN0_SIZE_FACTOR * (float)gc->memory_in_use) + GEN0_SIZE_ADDITION);
-  if(new_gen0_size > GEN0_MAX_SIZE)
+  if ((new_gen0_size > GEN0_MAX_SIZE)
+      || (gc->memory_in_use > GEN0_MAX_SIZE)) /* => overflow */
     new_gen0_size = GEN0_MAX_SIZE;
 
   resize_gen0(gc, new_gen0_size);
@@ -2205,6 +2245,34 @@ inline static void check_finalizers(NewGC *gc, int level)
 #include "weak.c"
 #undef is_marked
 #undef weak_box_resolve
+
+/*****************************************************************************/
+/* phantom bytes and accounting                                              */
+/*****************************************************************************/
+
+typedef struct {
+  short tag;
+  intptr_t count;
+} Phantom_Bytes;
+
+static int size_phantom(void *p, struct NewGC *gc)
+{
+  return gcBYTES_TO_WORDS(sizeof(Phantom_Bytes));
+}
+
+static int mark_phantom(void *p, struct NewGC *gc)
+{
+  Phantom_Bytes *pb = (Phantom_Bytes *)p;
+
+  gc->phantom_count = add_no_overflow(gc->phantom_count, pb->count);
+
+  return gcBYTES_TO_WORDS(sizeof(Phantom_Bytes));
+}
+
+static int fixup_phantom(void *p, struct NewGC *gc)
+{
+  return gcBYTES_TO_WORDS(sizeof(Phantom_Bytes));
+}
 
 /*****************************************************************************/
 /* Internal Stack Routines                                                   */
@@ -2729,7 +2797,9 @@ static void NewGC_initialize(NewGC *newgc, NewGC *inheritgc, NewGC *parentgc) {
 
 /* NOTE This method sets the constructed GC as the new Thread Specific GC. */
 static NewGC *init_type_tags_worker(NewGC *inheritgc, NewGC *parentgc, 
-                                    int count, int pair, int mutable_pair, int weakbox, int ephemeron, int weakarray, int custbox)
+                                    int count, int pair, int mutable_pair, int weakbox, 
+                                    int ephemeron, int weakarray, 
+                                    int custbox, int phantom)
 {
   NewGC *gc;
 
@@ -2744,6 +2814,7 @@ static NewGC *init_type_tags_worker(NewGC *inheritgc, NewGC *parentgc,
 # ifdef NEWGC_BTC_ACCOUNT
   gc->cust_box_tag    = custbox;
 # endif
+  gc->phantom_tag  = phantom;
 
   NewGC_initialize(gc, inheritgc, parentgc);
 
@@ -2763,6 +2834,7 @@ static NewGC *init_type_tags_worker(NewGC *inheritgc, NewGC *parentgc,
     GC_register_traversers2(gc->weak_box_tag, size_weak_box, mark_weak_box, fixup_weak_box, 0, 0);
     GC_register_traversers2(gc->ephemeron_tag, size_ephemeron, mark_ephemeron, fixup_ephemeron, 0, 0);
     GC_register_traversers2(gc->weak_array_tag, size_weak_array, mark_weak_array, fixup_weak_array, 0, 0);
+    GC_register_traversers2(gc->phantom_tag, size_phantom, mark_phantom, fixup_phantom, 0, 0);
   }
   initialize_signal_handler(gc);
   GC_add_roots(&gc->park, (char *)&gc->park + sizeof(gc->park) + 1);
@@ -2771,13 +2843,15 @@ static NewGC *init_type_tags_worker(NewGC *inheritgc, NewGC *parentgc,
   return gc;
 }
 
-void GC_init_type_tags(int count, int pair, int mutable_pair, int weakbox, int ephemeron, int weakarray, int custbox)
+void GC_init_type_tags(int count, int pair, int mutable_pair, int weakbox, int ephemeron, int weakarray, 
+                       int custbox, int phantom)
 {
   static int initialized = 0;
 
   if (!initialized) {
     initialized = 1;
-    init_type_tags_worker(NULL, NULL, count, pair, mutable_pair, weakbox, ephemeron, weakarray, custbox);
+    init_type_tags_worker(NULL, NULL, count, pair, mutable_pair, weakbox, ephemeron, weakarray, 
+                          custbox, phantom);
   } else {
     GCPRINT(GCOUTF, "GC_init_type_tags should only be called once!\n");
     abort();
@@ -2791,7 +2865,8 @@ struct NewGC *GC_get_current_instance() {
 #ifdef MZ_USE_PLACES
 void GC_construct_child_gc(struct NewGC *parent_gc, intptr_t limit) {
   NewGC *gc = MASTERGC;
-  NewGC *newgc = init_type_tags_worker(gc, parent_gc, 0, 0, 0, gc->weak_box_tag, gc->ephemeron_tag, gc->weak_array_tag, gc->cust_box_tag);
+  NewGC *newgc = init_type_tags_worker(gc, parent_gc, 0, 0, 0, gc->weak_box_tag, gc->ephemeron_tag, 
+                                       gc->weak_array_tag, gc->cust_box_tag, gc->phantom_tag);
   newgc->primoridal_gc = MASTERGC;
   newgc->dont_master_gc_until_child_registers = 1;
   if (limit)
@@ -2972,19 +3047,20 @@ void GC_register_traversers(short tag, Size_Proc size, Mark_Proc mark,
 intptr_t GC_get_memory_use(void *o) 
 {
   NewGC *gc = GC_get_GC();
-  intptr_t amt;
+  uintptr_t amt;
 #ifdef NEWGC_BTC_ACCOUNT
-  if(o) {
+  if (o) {
     return BTC_get_memory_use(gc, o);
   }
 #endif
-  amt = gen0_size_in_use(gc) + gc->memory_in_use;
+  amt = add_no_overflow(gen0_size_in_use(gc), gc->memory_in_use);
 #ifdef MZ_USE_PLACES
   mzrt_mutex_lock(gc->child_total_lock);
-  amt += gc->child_gc_total;
+  amt = add_no_overflow(amt, gc->child_gc_total);
   mzrt_mutex_unlock(gc->child_total_lock);
 #endif
-  return amt;
+  
+  return (intptr_t)amt;
 }
 
 /*****************************************************************************/
@@ -4255,7 +4331,7 @@ inline static void gen0_free_big_pages(NewGC *gc) {
 static void clean_up_heap(NewGC *gc)
 {
   int i;
-  size_t memory_in_use = 0;
+  uintptr_t memory_in_use = 0;
   PageMap pagemap = gc->page_maps;
 
   gen0_free_big_pages(gc);
@@ -4345,6 +4421,8 @@ static void clean_up_heap(NewGC *gc)
     }
     gc->med_freelist_pages[i] = prev;
   }
+
+  memory_in_use = add_no_overflow(memory_in_use, gc->phantom_count);
 
   gc->memory_in_use = memory_in_use;
   cleanup_vacated_pages(gc);
@@ -4532,6 +4610,9 @@ static void garbage_collect(NewGC *gc, int force_full, int switching_master, Log
      half the available memory */
   gc->in_unsafe_allocation_mode = 1;
   gc->unsafe_allocation_abort = out_of_memory_gc;
+
+  if (gc->gc_full)
+    gc->phantom_count = 0;
 
   TIME_INIT();
 
@@ -4811,7 +4892,7 @@ intptr_t GC_propagate_hierarchy_memory_use()
   }
 #endif
 
-  return gc->memory_in_use + gc->child_gc_total;
+  return add_no_overflow(gc->memory_in_use, gc->child_gc_total);
 }
 
 #if MZ_GC_BACKTRACE

src/racket/gc2/newgc.h
@@ -231,6 +231,9 @@ typedef struct NewGC {
   unsigned short weak_box_tag;
   unsigned short ephemeron_tag;
   unsigned short cust_box_tag;
+  unsigned short phantom_tag;
+
+  uintptr_t phantom_count;
 
   Roots roots;
   GC_Weak_Array *weak_arrays;
@@ -246,7 +249,7 @@ typedef struct NewGC {
   intptr_t previously_reported_total; /* how much we previously reported to the parent */
   mzrt_mutex *child_total_lock; /* lock on `child_gc_total' */
 #endif
-  intptr_t child_gc_total;
+  uintptr_t child_gc_total;
 
   uintptr_t place_memory_limit; /* set to propagate a custodian limit from a parent place */  
 

src/racket/src/salloc.c
@@ -113,7 +113,7 @@ void scheme_set_stack_base(void *base, int no_auto_statics) XFORM_SKIP_PROC
   GC_init_type_tags(_scheme_last_type_, 
                     scheme_pair_type, scheme_mutable_pair_type, scheme_weak_box_type, 
                     scheme_ephemeron_type, scheme_rt_weak_array,
-                    scheme_cust_box_type);
+                    scheme_cust_box_type, scheme_phantom_bytes_type);
   /* We want to be able to allocate symbols early. */
   scheme_register_traversers();
 #endif

samth/old-plt-new

commit 3945c8b61717a38e7b6a7f19678f6e1136aab45a

fix GC bug
The bug was introduced by commit aa08a68424, which added
support for phantom bytes.

src/racket/gc2/mem_account.c
@@ -462,6 +462,7 @@ static void BTC_do_accounting(NewGC *gc)
       last = cur;
       box = cur->global_next; cur = box ? SCHEME_PTR1_VAL(box) : NULL;
 
+      owner_table = gc->owner_table;
       owner_table[owner]->memory_use = add_no_overflow(owner_table[owner]->memory_use, 
                                                        gcBYTES_TO_WORDS(gc->phantom_count));
       gc->phantom_count = save_count;